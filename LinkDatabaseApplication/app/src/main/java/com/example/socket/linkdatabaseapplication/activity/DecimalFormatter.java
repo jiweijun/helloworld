7b22e97367393a9874996ec3426c64fd8f3f01e3
jiweijun <skyhome2015@163.com>
2019-06-04 13:29:00 +0800

    Create DecimalFormatter.java

---
+++ LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/DecimalFormatter.java
@@ -0,0 +1,21 @@
+package com.example.socket.linkdatabaseapplication.activity;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+import java.text.DecimalFormat;
+
+public class DecimalFormatter implements IAxisValueFormatter {
+    private DecimalFormat format;
+
+    public DecimalFormatter() {
+        format = new DecimalFormat("###,###,##0.00");
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        return format.format(value) + "$";
+    }
+
+
+}
