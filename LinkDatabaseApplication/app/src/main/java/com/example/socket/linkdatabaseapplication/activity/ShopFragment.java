e5221f628d698284b0ab3ce528166f330a1c6fd7
jiweijun <skyhome2015@163.com>
2019-06-04 13:29:07 +0800

    Update ShopFragment.java

--- LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/ShopFragment.java
+++ LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/ShopFragment.java
@@ -1,24 +1,70 @@
 package com.example.socket.linkdatabaseapplication.activity;
 
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.socket.linkdatabaseapplication.ChartActivity;
 import com.example.socket.linkdatabaseapplication.R;
+import com.example.socket.linkdatabaseapplication.scan;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
 
 
 public class ShopFragment extends BaseFragment {
 
+    private Button btn;
+    View view;
     public ShopFragment() {
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_shop, container, false);
+         view = inflater.inflate(R.layout.fragment_shop, container, false);
+        btn = (Button) view.findViewById(R.id.button4);
         return view;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent();
+
+                intent.setClass(getActivity(), ChartActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+
 
 
 }
