cce56bde04bb64dd7334b4d57ab3c1efc793d3e8
jiweijun <skyhome2015@163.com>
2019-07-04 13:57:10 +0800

    Update LiveFragment.java

--- LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/LiveFragment.java
+++ LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/LiveFragment.java
@@ -8,13 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.example.socket.linkdatabaseapplication.ChartActivity;
 import com.example.socket.linkdatabaseapplication.R;
-import com.example.socket.linkdatabaseapplication.scan;
+import com.example.socket.linkdatabaseapplication.WIFI.SocketActivity;
+import com.example.socket.linkdatabaseapplication.WIFI.wifiActivity;
+import com.example.socket.linkdatabaseapplication.barscan.scan;
 
 public class LiveFragment extends BaseFragment {
 
     private Button btn;
+    private Button btn_scan;
+    private Button btn_wifi;
     View view;
 
     public LiveFragment() {
@@ -24,6 +27,8 @@ public class LiveFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
          view=inflater.inflate(R.layout.fragment_live,container,false);
         btn = (Button) view.findViewById(R.id.btn_socket);
+        btn_scan = (Button) view.findViewById(R.id.btn_scan);
+        btn_wifi = (Button) view.findViewById(R.id.btn_wifi);
         return view;
     }
     @Override
@@ -44,15 +49,34 @@ public class LiveFragment extends BaseFragment {
                 startActivity(intent);
             }
         });
+
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent();
+
+                intent.setClass(getActivity(), scan.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btn_wifi.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent();
+
+                intent.setClass(getActivity(), wifiActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 
-    public void go2(View v) {
 
-
-        Intent intent = new Intent();
-
-        intent.setClass(getActivity(),scan.class);
-
-        startActivity(intent);
-    }
 }
