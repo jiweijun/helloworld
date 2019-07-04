262cca0ddd00124c3b1b73c5d2d49152bb8d414c
jiweijun <skyhome2015@163.com>
2019-07-04 13:58:19 +0800

    Create custom_marker_view.java

---
+++ LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/mpchart/custom_marker_view.java
@@ -0,0 +1,15 @@
+package com.example.socket.linkdatabaseapplication.mpchart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.socket.linkdatabaseapplication.R;
+
+public class custom_marker_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_marker_view);
+    }
+}
