7b8cd3968ddf2b4bc1cc65a216ecebb25044c8b2
jiweijun <skyhome2015@163.com>
2019-07-04 13:57:26 +0800

    Update ShopFragment.java

--- LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/ShopFragment.java
+++ LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/ShopFragment.java
@@ -2,31 +2,14 @@ package com.example.socket.linkdatabaseapplication.activity;
 
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.example.socket.linkdatabaseapplication.ChartActivity;
+import com.example.socket.linkdatabaseapplication.mpchart.ChartActivity;
 import com.example.socket.linkdatabaseapplication.R;
-import com.example.socket.linkdatabaseapplication.scan;
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.LimitLine;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-
-import java.util.ArrayList;
 
 
 public class ShopFragment extends BaseFragment {
